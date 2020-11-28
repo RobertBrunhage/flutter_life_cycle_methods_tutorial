6c4c05a488b3b001097bf73a02c92f3caaeaa7e1
robertbrunhage <robertbrunhage@gmail.com>
2020-11-28 06:10:19 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String title = 'Flutter Home Page';
+
+  void updateTitle() {
+    setState(() {
+      title = 'New Flutter Home Page';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(
+        title: title,
+        onPressed: updateTitle,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({
+    Key key,
+    this.title,
+    this.onPressed,
+  }) : super(key: key);
+
+  final String title;
+  final VoidCallback onPressed;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO: implement initState
+    print('initState');
+    _animationController = AnimationController(vsync: this);
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    print('didChangeDependencies');
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(covariant MyHomePage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // TODO: implement didUpdateWidget
+    print('didUpdateWidget');
+    if (this.widget.title != oldWidget.title) {
+      print('Title has changed');
+    }
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    print('deactivate');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    print('dispose');
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            ElevatedButton(
+              onPressed: widget.onPressed,
+              child: Text('DidUpdateWidget'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => ExampleRoutedPage()),
+                );
+              },
+              child: Text('Dispose'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExampleRoutedPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
